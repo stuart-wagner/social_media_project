6e4451deac10c4eba4791d3c5d004b56c48d6198
stuart-wagner <ww289546@icloud.com>
2025-12-11 19:49:01 -0600

    static file update

--- FeedProject/settings.py
+++ FeedProject/settings.py
@@ -126,6 +126,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Define a root for static files when running collectstatic (for production)
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
+# Define directories where Django should look for static files 
+# (in addition to app/static folders)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
